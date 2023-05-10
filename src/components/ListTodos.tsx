ebc94db5f49110da15a1451c14587e94afbc9c67
Dtheran <dtheran1@gmail.com>
2023-05-10 14:13:00 -0500

    validate todos

--- src/components/ListTodos.tsx
+++ src/components/ListTodos.tsx
@@ -18,98 +18,115 @@ export const ListTodos: React.FC<Props> = ({
 }) => {
   return (
     <div>
-      <div className='relative overflow-x-auto'>
-        <table className='w-full text-lg table-fixed text-left text-gray-900 '>
-          <thead className='text-white uppercase bg-gradient-to-b from-gray-400 to-gray-800'>
-            <tr>
-              <th scope='col' className='px-6 py-3'>
-                Done
-              </th>
-              <th scope='col' className='px-6 py-3'>
-                Title
-              </th>
-              <th scope='col' className='px-6 py-3 truncate'>
-                Category
-              </th>
-              <th scope='col' className='px-6 py-3 truncate'>
-                Description
-              </th>
-              <th scope='col' className='px-6 py-3 truncate'>
-                Actions
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {todos.map(todo => (
-              <tr key={todo.id} className='border-b text-white font-semibold'>
-                <th
-                  scope='row'
-                  className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap '>
-                  <div className='flex items-center'>
-                    {todo.status
-                      ? (
-                      <div
-                        onClick={() => {
-                          handleSetComplete(todo.id)
-                        }}
-                        className='bg-green-700 p-1 w-7 h-7 rounded-full cursor-pointer'>
-                        <img
-                          className='h-5 w-5'
-                          src={checkIcon}
-                          alt='Check Icon'
-                        />
-                      </div>
-                        )
-                      : (
-                      <span
-                        onClick={() => {
-                          handleSetComplete(todo.id)
-                        }}
-                        className={
-                          'border border-gray-800 border-solid p-3 rounded-full cursor-pointer'
-                        }></span>
-                        )}
-                  </div>
+      <div className='relative'>
+        {!todos.length
+          ? (
+          <h2 className='text-center text-4xl text-gray-700 font-semibold'>
+            Great, you don't have toDo it!
+          </h2>
+            )
+          : (
+          <table className='w-full text-lg table-fixed text-left text-gray-900 '>
+            <thead className='text-white uppercase bg-gradient-to-b from-gray-400 to-gray-800'>
+              <tr>
+                <th scope='col' className='px-6 py-3'>
+                  Done
                 </th>
-                <td className='px-6 py-4 truncate'>
-                  <p className={todo.status ? 'line-through text-green-600' : ''}>
-                    {todo.title}
-                  </p>
-                </td>
-                <td className='px-6 py-4 truncate'>
-                  <p className={todo.status ? 'line-through text-green-600' : ''}>
-                    {todo.category}
-                  </p>
-                </td>
-                <td className='px-6 py-4 w-20 truncate'>
-                  <p className={todo.status ? 'line-through text-green-600' : ''}>
-                    {todo.description}
-                  </p>
-                </td>
-                <td className='px-3 md:px-6 py-2 md:py-4 flex flex-col md:flex-row gap-2'>
-                  <button
-                    onClick={() => {
-                      handleUpdate(todo)
-                    }}
-                    className='border rounded-md w-10 p-2 bg-yellow-200 hover:scale-110'>
-                    <img className='h-5 w-5' src={editIcon} alt='Edit Icon' />
-                  </button>
-                  <button
-                    onClick={() => {
-                      handleDelete(todo.id)
-                    }}
-                    className='border rounded-md w-10 p-2 bg-red-200 hover:scale-110'>
-                    <img
-                      className='h-5 w-5'
-                      src={deleteIcon}
-                      alt='Delete Icon'
-                    />
-                  </button>
-                </td>
+                <th scope='col' className='px-6 py-3'>
+                  Title
+                </th>
+                <th scope='col' className='px-6 py-3 truncate'>
+                  Category
+                </th>
+                <th scope='col' className='px-6 py-3 truncate'>
+                  Description
+                </th>
+                <th scope='col' className='px-6 py-3 truncate'>
+                  Actions
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {todos.map(todo => (
+                <tr key={todo.id} className='border-b text-white font-semibold'>
+                  <th
+                    scope='row'
+                    className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap '>
+                    <div className='flex items-center'>
+                      {todo.status
+                        ? (
+                        <div
+                          onClick={() => {
+                            handleSetComplete(todo.id)
+                          }}
+                          className='bg-green-700 p-1 w-7 h-7 rounded-full cursor-pointer'>
+                          <img
+                            className='h-5 w-5'
+                            src={checkIcon}
+                            alt='Check Icon'
+                          />
+                        </div>
+                          )
+                        : (
+                        <span
+                          onClick={() => {
+                            handleSetComplete(todo.id)
+                          }}
+                          className={
+                            'border border-gray-800 border-solid p-3 rounded-full cursor-pointer'
+                          }></span>
+                          )}
+                    </div>
+                  </th>
+                  <td className='px-6 py-4 truncate'>
+                    <p
+                      className={
+                        todo.status ? 'line-through text-green-600' : ''
+                      }>
+                      {todo.title}
+                    </p>
+                  </td>
+                  <td className='px-6 py-4 truncate'>
+                    <p
+                      className={
+                        todo.status ? 'line-through text-green-600' : ''
+                      }>
+                      {todo.category}
+                    </p>
+                  </td>
+                  <td className='px-6 py-4 w-20 truncate'>
+                    <p
+                      className={
+                        todo.status ? 'line-through text-green-600' : ''
+                      }>
+                      {todo.description}
+                    </p>
+                  </td>
+                  <td className='px-3 md:px-6 py-2 md:py-4 flex flex-col md:flex-row gap-2'>
+                    <button
+                      onClick={() => {
+                        handleUpdate(todo)
+                      }}
+                      className='border rounded-md w-10 p-2 bg-yellow-200 hover:scale-110'>
+                      <img className='h-5 w-5' src={editIcon} alt='Edit Icon' />
+                    </button>
+                    <button
+                      onClick={() => {
+                        handleDelete(todo.id)
+                      }}
+                      className='border rounded-md w-10 p-2 bg-red-200 hover:scale-110'>
+                      <img
+                        className='h-5 w-5'
+                        src={deleteIcon}
+                        alt='Delete Icon'
+                      />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+            )}
       </div>
     </div>
   )
